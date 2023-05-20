c1bcfe9eb789abd625abbb57009b9ce1e6dae210
gatikrushna911 <gatikrushna911@gmail.com>
2023-05-20 12:55:31 +0530

    configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //what file will be using tailwindcss classes. means what are the files this will read
+    //  "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
